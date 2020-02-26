e3700e5243c26fa63d3313bf3d73a3fb3a3aa63d
Carrier.Zhang <carrier.zhang@ef.com>
2020-02-26 18:14:06 +0800

    Add ConfigService Module (Eureka Server) & make adminService (Eureka Client) register in Eureka Server

--- apollo-biz/src/main/java/com/example/my/apollo/biz/config/BizConfig.java
+++ apollo-biz/src/main/java/com/example/my/apollo/biz/config/BizConfig.java
@@ -1,8 +1,11 @@
 package com.example.my.apollo.biz.config;
 
 import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
+import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 import com.google.common.reflect.TypeToken;
@@ -15,23 +18,33 @@ public class BizConfig {
 
     private static final int DEFAULT_ITEM_KEY_LENGTH = 128;
     private static final int DEFAULT_ITEM_VALUE_LENGTH = 20000;
+    private static final String LIST_SEPARATOR = ",";
+    private Splitter splitter = Splitter.on(LIST_SEPARATOR).omitEmptyStrings().trimResults();
 
     private Gson gson = new Gson();
-    private static final Type namespaceValueLengthOverrideTypeReference =
-        new TypeToken<Map<Long, Integer>>() {
-        }.getType();
-        
-    public Map<Long, Integer> namespaceValueLengthLimitOverride() {
-        String namespaceValueLengthOverrideString = "";//getValue("namespace.value.length.limit.override");
-        Map<Long, Integer> namespaceValueLengthOverride = Maps.newHashMap();//即 new HashMap<Long,Integer>();
-        if (!Strings.isNullOrEmpty(namespaceValueLengthOverrideString)) {
-          namespaceValueLengthOverride =
-              gson.fromJson(namespaceValueLengthOverrideString, namespaceValueLengthOverrideTypeReference);
+    private static final Type namespaceValueLengthOverrideTypeReference = new TypeToken<Map<Long, Integer>>() {
+    }.getType();
+
+    public List<String> eurekaServiceUrls() {
+        String configuration = "http://localhost:8060/eureka/";  //getValue("eureka.service.url", "");
+        if (Strings.isNullOrEmpty(configuration)) {
+            return Collections.emptyList();
         }
-    
+
+        return splitter.splitToList(configuration);
+    }
+
+    public Map<Long, Integer> namespaceValueLengthLimitOverride() {
+        String namespaceValueLengthOverrideString = "";// getValue("namespace.value.length.limit.override");
+        Map<Long, Integer> namespaceValueLengthOverride = Maps.newHashMap();// 即 new HashMap<Long,Integer>();
+        if (!Strings.isNullOrEmpty(namespaceValueLengthOverrideString)) {
+            namespaceValueLengthOverride = gson.fromJson(namespaceValueLengthOverrideString,
+                    namespaceValueLengthOverrideTypeReference);
+        }
+
         return namespaceValueLengthOverride;
-      }
-      
+    }
+
     public int itemKeyLengthLimit() {
         int limit = DEFAULT_ITEM_KEY_LENGTH;
         return checkInt(limit, 5, Integer.MAX_VALUE, DEFAULT_ITEM_KEY_LENGTH);
--- apollo-configservice/src/main/java/com/example/my/apollo/configservice/ConfigServiceApplication.java
+++ apollo-configservice/src/main/java/com/example/my/apollo/configservice/ConfigServiceApplication.java
@@ -1,27 +1,29 @@
-package com.example.my.apollo.adminservice;
+package com.example.my.apollo.configservice;
 
 import com.example.my.apollo.biz.ApolloBizConfig;
 import com.example.my.apollo.common.ApolloCommonConfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableEurekaClient
+@EnableEurekaServer
+@EnableAspectJAutoProxy
+@EnableAutoConfiguration // (exclude = EurekaClientConfigBean.class)
 @Configuration
-@PropertySource(value = {"classpath:adminservice.properties"})//默认读classpath:application.properties,现在读参数里的文件
-@EnableAutoConfiguration
 @EnableTransactionManagement
-@ComponentScan(basePackageClasses = {ApolloCommonConfig.class,
-    ApolloBizConfig.class,
-    AdminServiceApplication.class})
-public class AdminServiceApplication {
+@PropertySource(value = { "classpath:configservice.properties" })
+@ComponentScan(basePackageClasses = { ApolloCommonConfig.class, 
+    ApolloBizConfig.class, 
+    ConfigServiceApplication.class })
+public class ConfigServiceApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(AdminServiceApplication.class,args);
+        SpringApplication.run(ConfigServiceApplication.class, args);
     }
 }
